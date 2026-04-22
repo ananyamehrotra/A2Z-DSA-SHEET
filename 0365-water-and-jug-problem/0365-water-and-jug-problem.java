5979e1ee64d8020e0e4cd0b81db9566d50c463e9
Ananya Mehrotra <ananyamehrotra2236@gmail.com>
2026-04-22 23:51:43 +0530

    Time: 0 ms (100%), Space: 41.8 MB (90.93%) - LeetHub

---
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.java
@@ -0,0 +1,10 @@
+class Solution {
+        public boolean canMeasureWater(int x, int y, int target) {
+        if (target > x + y) return false;
+        return target % gcd(x, y) == 0;
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
