267dfa1be2ea5d1555fe321b1b181445d99e15b9
Ananya Mehrotra <ananyamehrotra2236@gmail.com>
2026-07-03 20:10:09 +0530

    Time: 0 ms (100%), Space: 48.3 MB (30.97%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,45 +1,24 @@
 class Solution {
-    public int[] searchRange(int[] arr, int x) {
-        int lb=lowerBound(arr,x);
-        int ub=upperBound(arr,x);
-        if(lb==arr.length||arr[lb]!=x) return new int[]{-1,-1};
-        return new int[]{lb,ub-1};
-    }
-    public int lowerBound(int[] arr, int x){
-        int n = arr.length;
-        int low = 0;
-        int high = n - 1;
-        int floorIndex = arr.length;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            // If the middle element is a potential answer
-            if (arr[mid] >= x) {
-                floorIndex = mid; 
-                high = mid - 1;
-            } else {
-                low=mid+1;
-            }
+    public int[] searchRange(int[] nums, int target) {
+        int start = binarySearch(nums, target);
+        if (start == nums.length || nums[start] != target) {
+            return new int[]{-1, -1};
         }
-        return floorIndex;
+        int end = binarySearch(nums, target + 1) - 1;
+        
+        return new int[]{start, end};
     }
-    public int upperBound(int[] arr, int x){
-        int n = arr.length;
-        int low = 0;
-        int high = n - 1;
-        int ceilIndex = arr.length;
-
+    private int binarySearch(int[] nums, int val) {
+        int low = 0, high = nums.length - 1;
+        
         while (low <= high) {
             int mid = low + (high - low) / 2;
-
-            if (arr[mid] > x) {
-                ceilIndex = mid;
-                high = mid - 1; 
+            if (nums[mid] >= val) {
+                high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        return ceilIndex;
+        return low;
     }
 }
