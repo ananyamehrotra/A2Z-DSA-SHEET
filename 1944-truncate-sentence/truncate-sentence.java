cd29ea3998c2abb50002215306e8e08f09326de8
Ananya Mehrotra <ananyamehrotra2236@gmail.com>
2026-08-21 13:48:04 +0530

    Time: 1 ms (73.07%) | Memory: 42.9 MB (85.80%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String[] words = s.split(" ");
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < k; i++) {
+            if (i > 0)
+                ans.append(" ");
+            ans.append(words[i]);
+        }
+
+        return ans.toString();
+    }
+}
