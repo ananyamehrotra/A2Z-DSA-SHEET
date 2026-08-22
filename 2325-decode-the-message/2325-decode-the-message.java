9673cef021b7ec99b7eaac96e1068f57385a5301
Ananya Mehrotra <ananyamehrotra2236@gmail.com>
2026-08-22 18:15:37 +0530

    Time: 7 ms (67.57%), Space: 45.3 MB (45.33%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String decodeMessage(String key, String message) {
+        char[] m = new char[128];
+    m[' '] = ' ';
+    char cur = 'a';
+    for (var k : key.toCharArray())
+        m[k] = m[k] != 0 ? m[k] : cur++;
+    return message.chars().mapToObj(i -> String.valueOf(m[i])).collect(Collectors.joining());
+    }
+}
